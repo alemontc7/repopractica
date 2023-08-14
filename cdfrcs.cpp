4d0545a07034200c660893624c2465dbb1fdb881
Alejandro Montero Coca <alejandro.montero@ucb.edu.bo>
2023-08-14 03:17:25 -0400

    problem code added

---
+++ cdfrcs.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#define for(i, n) for(int i = 0; i < n; i++)
+#define int long long
+
+//pacha 2880 fan club
+
+#define CAPACITY 50000 // Size of the HashTable.
+
+
+using namespace std;
+signed main()
+{
+    int a; string s;
+    cin>>a;
+    while(a--)
+    {
+
+    }
+}
