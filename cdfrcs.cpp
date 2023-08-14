e88a19acbd1b486ba1b46102727ae2a3096148ae
Alejandro Montero Coca <alejandro.montero@ucb.edu.bo>
2023-08-14 03:26:45 -0400

    First approach

--- cdfrcs.cpp
+++ cdfrcs.cpp
@@ -2,21 +2,34 @@
 #include <vector>
 #include <map>
 #include <set>
-#define for(i, n) for(int i = 0; i < n; i++)
+#define f first
+#define s second
+#define forin(i, n) for(int i = 0; i < n; i++)
 #define int long long
 
 //pacha 2880 fan club
+#define tm 50000 
 
-#define CAPACITY 50000 // Size of the HashTable.
-
+unsigned long hashito(string str)
+{
+    unsigned long i = 0;
+    for (int j = 0; str[j]; j++)
+        i += str[j];
+    return i % tm;
+}
 
 using namespace std;
 signed main()
 {
-    int a; string s;
+    int a; string ss;
+    map<int, pair<int, string>> m;
     cin>>a;
     while(a--)
     {
-
+        cin>>ss;
+        m[hashito(ss)].f++;
+        m[hashito(ss)].s = ss; 
     }
+
+
 }
